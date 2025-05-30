2f9740074ad737d91fe647fba92232e3806c2e83
Aden <outdoor.co.se@gmail.com>
2025-05-30 14:01:51 +0200

    final changes

---
+++ Controllers/EventsController.cs
@@ -0,0 +1,35 @@
+﻿using EventAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EventAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        private static List<Event> events = new List<Event>
+        {
+            new Event { Id = 1, Name = "Tech Conference 2023", Description = "Annual technology conference", Date = DateTime.Parse("2023-11-15"), Location = "Stockholm", ImageUrl = "https://example.com/tech-conf.jpg" },
+            new Event { Id = 2, Name = "Developer Workshop", Description = "Hands-on coding workshop", Date = DateTime.Parse("2023-12-05"), Location = "Gothenburg", ImageUrl = "https://example.com/dev-workshop.jpg" }
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Event>> GetEvents()
+        {
+            return Ok(events);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Event> GetEvent(int id)
+        {
+            var eventItem = events.FirstOrDefault(e => e.Id == id);
+            if (eventItem == null)
+            {
+                return NotFound();
+            }
+            return Ok(eventItem);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Event API v1"));
+}
+
+app.UseHttpsRedirection();
+app.UseCors("AllowAll");
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
